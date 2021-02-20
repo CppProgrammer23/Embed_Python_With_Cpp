f580726949f38ed2a111efa2d7edcfbb7160161a
Idriss Chaouch <62848804+CppProgrammer23@users.noreply.github.com>
2021-02-20 09:34:56 +0100

    Update txtSpe.py

--- QRCode and Voice/txtSpe.py
+++ QRCode and Voice/txtSpe.py
@@ -1,24 +1,24 @@
 # This Python file uses the following encoding: utf-8
 
-#from gtts import gTTS
-#import playsound
-#import speech_recognition as sr
-#
-#
-#def talk(txt):
-#    tts = gTTS(text=txt, lang='en')
-#    tts.save('start.mp3')
-#    playsound.playsound('start.mp3')
-#
-#
-#def getAudio():
-#    a = sr.Recognizer()
-#    with sr.Microphone() as s:
-#        audio = a.listen(s)
-#        said = ''
-#        try:
-#            said = a.recognize_google(audio)
-#            #print(said)
-#        except Exception as e:
-#            print(e)
-#    return said.encode('ascii')
+from gtts import gTTS
+import playsound
+import speech_recognition as sr
+
+
+def talk(txt):
+    tts = gTTS(text=txt, lang='en')
+    tts.save('start.mp3')
+    playsound.playsound('start.mp3')
+
+
+def getAudio():
+    a = sr.Recognizer()
+    with sr.Microphone() as s:
+        audio = a.listen(s)
+        said = ''
+        try:
+            said = a.recognize_google(audio)
+            #print(said)
+        except Exception as e:
+            print(e)
+    return said.encode('ascii') #you hzve to encode it because C++ strings and Pyhton strings are not the same
